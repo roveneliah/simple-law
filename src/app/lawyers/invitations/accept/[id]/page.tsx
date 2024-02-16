8b6e46aba19da97718d69001d2a90e66858040c3
Eli <ehanover21@gmail.com>
2024-02-15 17:57:42 -0800

    service agreement agreeement

--- src/app/lawyers/invitations/accept/[id]/page.tsx
+++ src/app/lawyers/invitations/accept/[id]/page.tsx
@@ -102,7 +102,7 @@ export default function OffersView() {
       return
     }
 
-    if (formData.getAll('agreements').length !== agreements.length) {
+    if (formData.getAll('agreements').length !== agreements.length + 1) {
       alert('Please agree to all the agreements.')
       return
     }
@@ -252,6 +252,37 @@ export default function OffersView() {
                       </div>
                     </div>
                   ))}
+                  <div
+                    key={agreements.length}
+                    className="relative flex gap-x-3"
+                  >
+                    <div className="flex h-6 items-center">
+                      <input
+                        id={`agreement-${agreements.length}`}
+                        name="agreements"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                    </div>
+                    <div className="text-sm leading-6">
+                      <label
+                        htmlFor={`agreement-${agreements.length}`}
+                        className="font-medium text-gray-900"
+                      >
+                        Impossible Agreement
+                      </label>
+                      <p className="text-gray-500">
+                        I have read and agree to ImpossibleLaw's{' '}
+                        <a
+                          href="www.google.com"
+                          className="font-bold text-indigo-600"
+                        >
+                          Service Agreement
+                        </a>
+                        .
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </fieldset>
             </div>
