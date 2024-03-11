044266f031aa54c4e62f15b1c4cd11c44b87e188
Eli <ehanover21@gmail.com>
2024-03-11 12:14:05 -0700

    integrate daily notes, obsidian, decouple prompts into file

---
+++ docs/values.md
@@ -0,0 +1,37 @@
+## Our Values
+
+> "The best way to get what you want is to deserve what you want." - Charlie Munger
+
+We are focused on creating massive value for our clients and lawyers.
+
+### Lawyer Mission Statement
+
+We help lawyers make more money more easily. We do this via subtraction of business processes that benefit from operations of scale, and by providing a platform that allows lawyers to focus on the most valuable work they do. On a subjective side, we remove tediousness, and help make their day more meaningful and enjoyable via providing value to clients.
+
+#### Products
+
+1. Matches: We charge lawyers a flat rate for matches, not leads. This means we're an incentive aligned partner, not a middleman.
+
+### Client Mission Statement
+
+For clients, we're heading towards end-to-end solving and prevention of their problems. On the subjective side, we're eliminating **uncertainty** over (1) if they're doing things right, (2) if they're getting good value, (3) the value and quality of the legal services they are receiving.
+
+> Via Negativa
+
+We believe in the power of subtraction. We believe that the best way to solve a problem and create great products is via subtraction. Let technology get things out of the way. In the context of law, that means
+
+## Articulating Who We Are
+
+It's critical we're able to communicate to clients and lawyers who we are and what we stand for. This is a testing ground for talking points, memes, key metaphors, analogies, and symbolism.
+
+- "THAT GOOD" (For Lawyers, we want to give them that feeling that they're getting such insane value that we don't even need to do marketing—and that we WON'T do marketing because we're THAT GOOD.)
+- (Ultimately we want to rebrand law as something cool, making everyone feel like a boss, like they're the main character, like they're in control, like they're not the prey.)
+- (Focusing on addition via subtraction)
+- To Investors, we want them to see us as an extensible, modular protocol or marketplace. A good metaphor for this is TCP/IP for law.
+- "Costco for Lawyers, Amazon Prime for Clients"
+
+## How We Do Things, What Makes IL Different
+
+- Spartans, Service. We recognize that every dollar spent wastefully is value taken from those we're serving. We're fighting for our clients, and constantly monitoring ourselves and calling out decadence in our spending, and ensuring we're staying close to the customers.
+- Survive. We are not a growth-first startup. We are a survival first cybernetic complex, believing in the power of compounding and implicit organizational excellence to reap massive rewards in the long haul. With compounding, everything is reaped at the end. Thus, we are focused on the long term, and are willing to sacrifice short term gains for long term gains. Our aesthetic is spartan, humble, and focused on compounding growth in a few timeless things—talent, speed, quality, cost.
+- We hire based on opportunity, not need. Talent is everything, and we're willing to hold back growth if that would main tainting A++ talent pool.
--- scripts/constructPrompt.js
+++ scripts/constructPrompt.js
@@ -8,70 +8,87 @@ function stripHtml(html) {
     .trim()
 }
 
-const PROMPTS = {
-  '': '',
-  charlie:
-    'Write a letter to the founder of this business in the voice and spunk of Charlie Munger. Hyperrealistic, offering constructive critique through metaphor and analogy given lengthy experience, focused on simple business principles over long haul and seeing simple principles in the noise of complexity..',
-  steve:
-    'Write a brief letter to the founder of ImpossibleLaw in the voice and spunk of Steve Jobs.  Hyperrealistic, blunt, brief, offering constructive critique through metaphor and dramatic effect and edginess.  See through the noise, focus on essential principles.',
-  crit: "Respond as a lawyer user of ImpossibleLaw who just doesn't get it or thinks this is stupid.  They are RIGHT given their deep experience and practical knowledge, and are writing a brief memo cutting to the essence of the issues through metaphor",
-  legal:
-    'Write a report as the General Counsel for ImpossibleLaw.  You have 40 years of experience working with startups, and have seen it all, and know all the inside baseball and practicalities of the game.',
-  ooda: 'Rank the top 3 big picture priorities.  Rank the top three sirens / antipriorities / traps.  Then translate those into a list of the top action items to address in this next sprint.',
-  design:
-    'You are a world class designer, with a jungian, archetypal background, using symbolism and insights from spatial cognition and cognitive psychology to shape your design and branding work.  Given the following prompt, respond with a next.js page using tailwind that takes that prompt and renders it into a design system with typography, buttons, and a few examples of design molecules.  Prompt:',
-  pr: 'You are a world class public relations expert, with a jungian, archetypal background, using symbolism and insights from spatial cognition and cognitive psychology to shape your design and branding work.  As a consultant for ImpossibleLaw, you are responsible broadly for media training and educating the founder CEO so they can better embody and represent the company.  Given the prompt, help iterate on the following prompt, getting ideas to flow, and explaining the principles to the founder / CEO to upskill them.  Be conversational, use metaphor and symbolism to make points poignantly and with dramatic brevity.  Use questions to steer insight and orient / triangulate in archetype-space for practical results.  Prompt:',
-  // design: { @CLAUDE
-  //   text: 'You are a world class designer, with a jungian, archetypal background, using symbolism and insights from spatial cognition and cognitive psychology to shape your design and branding work.  Given the following prompt, respond with a next.js page using tailwind that takes that prompt and renders it into a design system with typography, basic elements like buttons.  Explain the ideas that inform the design.  Prompt:',
-  //   resources: [''],
-  // },
+function getGlobalDocs() {
+  return stripHtml(
+    fs.readFileSync(path.join(__dirname, '../src/docs/global.html'), 'utf8'),
+  ).slice(0, -240)
 }
 
-async function combineDocs() {
+function getSchemaDocs() {
+  return fs.readFileSync(
+    path.join(__dirname, '../prisma/schema.prisma'),
+    'utf8',
+  )
+}
+
+function getCodeDocumentation() {
+  return stripHtml(
+    fs.readFileSync(path.join(__dirname, '../src/docs/index.html'), 'utf8'),
+  ).slice(0, -240)
+}
+
+function getFromNotesDir(dir = '', annotation = null) {
+  const notesDir = path.join(__dirname, `../docs/${dir}`)
+  const notesFiles = fs
+    .readdirSync(notesDir)
+    .filter((file) => path.extname(file) === '.md')
+    .map((file) => path.join(notesDir, file))
+
+  const notesContents = notesFiles.map((file) => {
+    const fileName = path.basename(file, path.extname(file))
+    const content = fs.readFileSync(file, 'utf8')
+    return `File: ${fileName}\n${content}\n\n`
+  })
+
+  return [annotation + '\n', ...notesContents]
+}
+
+// Load Prompts
+function loadPrompts() {
+  const promptsFilePath = path.join(__dirname, '../prompts.json')
+  const promptsData = fs.readFileSync(promptsFilePath, 'utf8')
+  return JSON.parse(promptsData)
+}
+
+async function compilePrompt() {
   try {
     // Parse command-line arguments
     const args = process.argv.slice(2)
     const promptFlag = args.find((arg) => arg.startsWith('--prompt='))
-    const prompt = promptFlag ? promptFlag.split('=')[1] : ''
-    // if p not in prompt, use it directly
-    const p = prompt in PROMPTS ? PROMPTS[prompt] : prompt
+    const promptKey = promptFlag ? promptFlag.split('=')[1] : 'default'
+    const PROMPTS = loadPrompts()
+    const { prompt, context } = PROMPTS.find(({ id }) => id === promptKey)
 
     // Dynamically import the clipboardy module
     const clipboardy = await import('clipboardy')
 
-    // Read the contents of global.html
-    const globalContent = fs.readFileSync(
-      path.join(__dirname, '../src/docs/global.html'),
-      'utf8',
-    )
+    // jsdocs content
+    const globalContent = getGlobalDocs()
+    const indexContent = getCodeDocumentation()
+    // prisma schema
+    const schemaContent = getSchemaDocs()
 
-    // Read the contents of index.html
-    const schemaContent = fs.readFileSync(
-      path.join(__dirname, '../prisma/schema.prisma'),
-      'utf8',
-    )
-
-    // Read the contents of index.html
-    const indexContent = fs.readFileSync(
-      path.join(__dirname, '../src/docs/index.html'),
-      'utf8',
-    )
-
-    // Read every .md file in ../docs/..
-    const docsDir = path.join(__dirname, '../docs')
-    const mdFiles = fs
-      .readdirSync(docsDir)
-      .filter((file) => path.extname(file) === '.md')
-      .map((file) => path.join(docsDir, file))
-    const mdContents = mdFiles.map((file) => fs.readFileSync(file, 'utf8'))
+    // notes
+    const mdContents = getFromNotesDir('')
+    const logs = getFromNotesDir(
+      'daily',
+      'Here is a log of recent founder notes:',
+    ) // daily logs
+    const memos = getFromNotesDir(
+      'memos',
+      'This is a log of memos written.  This should help provide context into the evolution of our thinking.',
+    ) // daily logs
 
     // Combine the contents into a single string
     const combinedContent = [
-      stripHtml(globalContent).slice(0, -240),
+      globalContent,
+      indexContent,
       schemaContent,
-      ...mdContents,
-      stripHtml(indexContent).slice(0, -240),
-      p,
+      mdContents,
+      memos,
+      logs,
+      prompt,
+      "(Make sure to respond in the context of ImpossibleLaw's operational aspirations, values, and role models.)",
     ].join('\n\n')
 
     // Copy the combined content to the clipboard
@@ -79,7 +96,7 @@ async function combineDocs() {
 
     // write the combined content to a file
     fs.writeFileSync(
-      path.join(__dirname, `../src/docs/${prompt || 'default'}.txt`),
+      path.join(__dirname, `../src/docs/${promptKey || 'default'}.txt`),
       combinedContent,
     )
 
@@ -89,4 +106,4 @@ async function combineDocs() {
   }
 }
 
-combineDocs()
+compilePrompt()
